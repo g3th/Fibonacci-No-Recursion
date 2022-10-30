47fb7206a0380fa05af5e63ea42eb00a841bd566
Roberto <mrrobertrausch@gmail.com>
2022-10-30 04:38:17 +0000

    added another function to compute sequence 	modified:   fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -6,7 +6,7 @@ void fibonacci (int);
 
 int main(){
 	// comment this out if calling 'fibonacci'.
-	/*int n1 =0;
+	int n1 =0;
 	int n2 =1;
 	int sum;
 	int counter;
@@ -21,10 +21,10 @@ int main(){
 		cout << sum << " ";
 		fibonacci_swap(n1, n2, sum);
 		++counter;
-	}*/
+	}
 
 	// uncomment this if calling 'fibonacci_swap'
-	fibonacci (15);
+	//fibonacci (15);
 }
 
 int fibonacci_swap(int& n1, int& n2, int temp){
